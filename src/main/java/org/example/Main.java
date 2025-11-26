327191cdae4b7ae45ce28d489f83e25e4cc6b058
Amaan0107 <Adilawar2007@gmail.com>
2025-11-26 09:43:54 -0800

    Example commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Fun Fact:");
+        System.out.println("Hello Everyone");
     }
 }
