b5c5e12130a5b2b70c1e143fab18ae7e12b5d6bd
Amaan0107 <Adilawar2007@gmail.com>
2025-11-26 09:45:03 -0800

    Example commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Fun Fact:");
-        System.out.println("Hello Everyone");
     }
 }
